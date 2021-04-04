60a1f908acebaaa46b25eaf225abcd6b55771780
rela589n <zsidelnik@gmail.com>
2021-04-04 20:55:53 +0300

    test(Serializer/Composed): ComposedDeserializerFactoryContextTest and ComposedSerializerFactoryContextTest

---
+++ tests/Unit/Serializer/Composed/Factory/ComposedDeserializerFactoryContextTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Serializer\Composed\Factory;
+
+use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Rela589n\DoctrineEventSourcing\Entity\AggregateRoot;
+use Rela589n\DoctrineEventSourcing\Serializer\Composed\ComposedDeserializer\Factory\Context;
+
+/**
+ * @covers \Rela589n\DoctrineEventSourcing\Serializer\Composed\ComposedDeserializer\Factory\Context
+ */
+final class ComposedDeserializerFactoryContextTest extends TestCase
+{
+    private MockObject|EntityManagerInterface $entityManager;
+    private MockObject|AggregateRoot $entity;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->entityManager = $this->createMock(EntityManagerInterface::class);
+        $this->entity = $this->createMock(AggregateRoot::class);
+    }
+
+    public function testHoldsGivenData(): void
+    {
+        $context = Context::make()
+            ->withEntityManager($this->entityManager)
+            ->withEntity($this->entity)
+            ->withPropertiesMeta(['properties' => 'meta'])
+            ->withCastArgumentsMap(['cast' => 'map']);
+
+        self::assertSame($this->entityManager, $context->getManager());
+        self::assertSame($this->entity, $context->getEntity());
+        self::assertSame(['properties' => 'meta'], $context->getPropertiesMeta());
+        self::assertSame(['cast' => 'map'], $context->getCastArgumentsMap());
+    }
+}
